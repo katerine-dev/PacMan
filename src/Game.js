87d9ce8bc1e060f04ab5aadc000851a26724d43b
katerine-dev <ka.lwitkoski@gmail.com>
2025-05-27 21:10:14 -0300

    Finaliza o jogo criando uma tela de game over e winner

--- src/Game.js
+++ src/Game.js
@@ -17,6 +17,7 @@ const gameWinSound = new Audio('../sounds/gameWin.wav');
 
 function gameLoop() {
   tileMap.draw(ctx);
+  drawGameEnd();
   pacman.draw(ctx, pause(), enemies);
   enemies.forEach((enemy) => enemy.draw(ctx, pause(), pacman));
   checkGameOver();
@@ -50,6 +51,28 @@ function isGameOver(){
 function pause() {
   return !pacman.madeFirstMove || gameOver || gameWin;
 }
+
+function drawGameEnd() {
+  if (gameOver || gameWin) {
+    let text = " You Win!";
+    if (gameOver) {
+      text = "Game Over";
+    }
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, canvas.height / 3.2, canvas.width, 80);
+
+    ctx.font = "75px comic sans";
+    const gradient = ctx.createLinearGradient(0, 0, canvas.width, 0);
+    gradient.addColorStop("0", "magenta");
+    gradient.addColorStop("0.5", "blue");
+    gradient.addColorStop("1.0", "red");
+
+    ctx.fillStyle = gradient;
+    ctx.fillText(text, 10, canvas.height / 2);
+  }
+}
+
 
 tileMap.setCanvasSize(canvas);
 setInterval(gameLoop, 1000 / 75);
